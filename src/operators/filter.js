e338613c348a4eb3884edbbdd494c73653e51c4a
Maurice <maurice@MacBook-Air-de-Maurice-dominguez.local>
2023-06-14 13:06:51 +0200

    feat: adds filter operator

--- src/operators/filter.js
+++ src/operators/filter.js
@@ -1,5 +1,15 @@
 import Observable from '../core/Observable';
 
-export const filter = (/*p*/) => (/*observable*/) => {
-  return new Observable((/*observer*/) => {});
+export const filter = (p) => (observable) => {
+  return new Observable((observer) => {
+    const sub = observable.subscribe(
+      (x) => {
+        if (p(x)) observer.next(x);
+      },
+      (e) => observer.error(e),
+      () => observer.complete()
+    );
+
+    return () => sub.unsubscribe();
+  });
 };
