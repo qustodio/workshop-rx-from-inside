9c93b3005e06af3458b78919fee184a935096f0c
Maurice <maurice@MacBook-Air-de-Maurice-dominguez.local>
2023-06-14 11:37:56 +0200

    feat: adds of operator

--- src/creation/of.js
+++ src/creation/of.js
@@ -1,5 +1,9 @@
 import Observable from '../core/Observable';
 
-export const of = (/*x*/) => {
-  return new Observable((/*observer*/) => {});
+export const of = (x) => {
+  return new Observable((observer) => {
+    observer.next(x);
+    observer.complete();
+    () => {};
+  });
 };
--- src/creation/of.spec.js
+++ src/creation/of.spec.js
@@ -1,6 +1,6 @@
 import { of } from './of';
 
-describe.skip('creation/of', () => {
+describe('creation/of', () => {
   it('should put as value into observable', () => {
     return expect(of('A')).toSubscribe(({ next, complete }) => [
       next('A'),
