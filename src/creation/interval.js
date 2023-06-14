a9091216fc70952dbd35f148e0afd96a0391358d
Sandra Sanz <sandra.sanz@qustodio.com>
2023-06-14 12:09:57 +0200

    feat: interval creation function
    Co-authored-by: Roberto Naharro <roberto-naharro@users.noreply.github.com>
    Co-authored-by: Maurice Ronet Dominguez <maurice.ronet.dominguez@gmail.com>

--- src/creation/interval.js
+++ src/creation/interval.js
@@ -1,5 +1,15 @@
 import Observable from '../core/Observable';
 
-export const interval = (/*ms*/) => {
-  return new Observable((/*observer*/) => {});
+export const interval = (ms) => {
+  return new Observable((observer) => {
+    let i = 0;
+    const interval = setInterval(() => {
+      observer.next(i);
+      i++;
+    }, ms);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
 };
