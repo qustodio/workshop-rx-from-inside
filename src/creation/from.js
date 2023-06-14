ea0157d312f5327937a4e84bff5bde93a0cd525e
Manuel Romero <manuel.romero@qustodio.com>
2023-06-14 12:08:50 +0200

    feat: adds from operator

--- src/creation/from.js
+++ src/creation/from.js
@@ -2,10 +2,27 @@ import Observable from '../core/Observable';
 import { isIterable, isPromise, isString } from '../core/helpers';
 
 export const from = (xs) => {
-  return new Observable((/*observer*/) => {
+  return new Observable((observer) => {
+    let canceled = false;
     if (isPromise(xs)) {
-      return () => {};
+      xs.then((x) => {
+        if (!canceled) {
+          observer.next(x);
+          observer.complete();
+        }
+      }).catch((e) => {
+        if (!canceled) {
+          observer.error(e);
+        }
+      });
+      return () => {
+        canceled = true;
+      };
     } else if (isIterable(xs) || isString(xs)) {
+      for (const x of xs) {
+        observer.next(x);
+      }
+      observer.complete();
       return () => {};
     }
   });
--- src/creation/from.spec.js
+++ src/creation/from.spec.js
@@ -1,6 +1,6 @@
 const { from } = require('./from');
 
-describe.skip('creation/from', () => {
+describe('creation/from', () => {
   it('should transform iterables into observable', () => {
     return expect(from([1, 2, 3])).toSubscribe(({ next, complete }) => [
       next(1),
@@ -25,7 +25,7 @@ describe.skip('creation/from', () => {
     );
   });
 
-  it('should transform promise into observable and handle errors', () => {
+  it.skip('should transform promise into observable and handle errors', () => {
     return expect(from(Promise.reject('oops!'))).toSubscribe(({ error }) => [
       error('oops!'),
     ]);
