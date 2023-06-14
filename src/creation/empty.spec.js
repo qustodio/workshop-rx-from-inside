b523a7c40630aa3e47bb5749f9463ff16c66d159
Sandra Sanz <sandra.sanz@qustodio.com>
2023-06-14 12:00:04 +0200

    feat: empty creation function
    Co-authored-by: Roberto Naharro <roberto-naharro@users.noreply.github.com>

--- src/creation/empty.spec.js
+++ src/creation/empty.spec.js
@@ -1,6 +1,6 @@
 const { empty } = require('./empty');
 
-describe.skip('creation/empty', () => {
+describe('creation/empty', () => {
   it('should not emit values', () => {
     return expect(empty()).toSubscribe(({ complete }) => [complete()]);
   });
