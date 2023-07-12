a7137f44bebe33089d2643910e57d6505a71a21b
Maurice <maurice@MacBook-Air-de-Maurice-dominguez.local>
2023-07-12 11:38:57 +0200

    feat: adds switchMap operator

--- src/operators/switchMap.js
+++ src/operators/switchMap.js
@@ -1,5 +1,41 @@
 import Observable from '../core/Observable';
 
-export const switchMap = (/*f*/) => (/*observable*/) => {
-  return new Observable((/*observer*/) => {});
+export const switchMap = (f) => (observable) => {
+  return new Observable((observer) => {
+    let innerSubscription;
+    let subscription;
+    let isCompletedInner = false;
+    let isCompletedOuter = false;
+
+    subscription = observable.subscribe(
+      (data) => {
+        innerSubscription?.unsubscribe();
+
+        const innerObservable = f(data);
+
+        innerSubscription = innerObservable.subscribe(
+          (innerData) => observer.next(innerData),
+          (e) => observer.error(e),
+          () => {
+            isCompletedInner = true;
+            if (isCompletedInner && isCompletedOuter) {
+              observer.complete();
+            }
+          }
+        );
+      },
+      (e) => observer.error(e),
+      () => {
+        isCompletedOuter = true;
+        if (isCompletedInner && isCompletedOuter) {
+          observer.complete();
+        }
+      }
+    );
+
+    return () => {
+      subscription.unsubscribe();
+      innerSubscription?.unsubscribe();
+    };
+  });
 };
--- src/operators/switchMap.spec.js
+++ src/operators/switchMap.spec.js
@@ -1,7 +1,7 @@
 import { switchMap } from './switchMap';
 import { seq } from '../creation/seq';
 
-describe.skip('operators/switchMap', () => {
+describe('operators/switchMap', () => {
   it('should flat observables values', () => {
     const numbers = seq([1, 2, 3], 50);
     const pairs = switchMap((x) =>
@@ -19,6 +19,27 @@ describe.skip('operators/switchMap', () => {
       next([1, 2]),
       next([2, 1]),
       next([2, 2]),
+      next([3, 1]),
+      next([3, 2]),
+      complete(),
+    ]);
+  });
+
+  it('should cancel observables values', () => {
+    const numbers = seq([1, 2, 3], 50);
+    const pairs = switchMap((x) =>
+      seq(
+        [
+          [x, 1],
+          [x, 2],
+        ],
+        60
+      )
+    )(numbers);
+
+    return expect(pairs).toSubscribe(({ next, complete }) => [
+      next([1, 1]),
+      next([2, 1]),
       next([3, 1]),
       next([3, 2]),
       complete(),
